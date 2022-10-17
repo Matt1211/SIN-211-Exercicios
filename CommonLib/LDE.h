f241aaabf53c80e2cee00cb5687899d0640d92cd
Matheus Moreira <matt4git@gmail.com>
2022-10-16 23:14:51 -0300

    Finalizando biblioteca da LDE

--- CommonLib/LDE.h
+++ CommonLib/LDE.h
@@ -84,3 +84,69 @@ void inserir_fim(No **lista, int elemento)
         }
     }
 }
+
+void remover_inicio(No **lista)
+{
+    No *to_be_removed;
+    to_be_removed = *lista;
+
+    if (!is_lista_vazia(*lista))
+    {
+        *lista = to_be_removed->dir;
+        if (*lista != NULL)
+        {
+            (*lista)->esq = NULL;
+        }
+        DesalocaNo(to_be_removed);
+    }
+
+    printf("\nErro: lista vazia!\n");
+    exit(1);
+}
+
+No *pesquisar(No *lista, int elemento)
+{
+    No *aux;
+    aux = lista;
+
+    if (!is_lista_vazia(lista))
+    {
+        while (aux != NULL)
+        {
+            if (aux->info == elemento)
+            {
+                return aux;
+            }
+            aux = aux->dir;
+        }
+    }
+
+    printf("\nNo nao encontrado\n");
+    return NULL;
+}
+
+int remover_valor(No **lista, int elemento)
+{
+    No *to_be_removed;
+
+    if ((to_be_removed = pesquisar(*lista, elemento)) != NULL)
+    {
+        if (*lista == to_be_removed)
+        {
+            remover_inicio(lista);
+        }
+        else
+        {
+            (to_be_removed->esq)->dir = to_be_removed->dir;
+            if (to_be_removed->dir != NULL)
+            {
+                (to_be_removed->dir)->esq = to_be_removed->esq;
+            }
+            DesalocaNo(to_be_removed);
+        }
+
+        return 1;
+    }
+
+    return 0;
+}
